bb935acaf79d00f7b27ec88753c54bf782f7c647
Quinn Murphy <gamefiend@github.com>
2017-03-16 14:21:19 -0400

    Added testing scaffold, refined issues.py

--- issues.py
+++ issues.py
@@ -1,49 +1,52 @@
 from github import Github
-from env import GITHUB_TOKEN, DEFAULT_ORG, USER
-from gh_format import * 
+from env import GITHUB_TOKEN, DEFAULT_ORG
+from gh_format import *
 from readchar import readkey
 
 g = Github(GITHUB_TOKEN)
 
+
 def gh_init(gh):
-	return g.get_organization(DEFAULT_ORG)
-		
-def get_issues(option,gh):
-	return gh.get_issues(filter=option,state='open')
+    return g.get_organization(DEFAULT_ORG)
+
+
+def get_issues(option, gh):
+    return gh.get_issues(filter=option, state='open')
+
 
 def display_issues(issues):
     display = PrettyTable(["Title", "Link", "Description"])
     for i in issues:
-        display.add_row([print_display(i.title),print_url(i.url),i.body[:30]])
+        display.add_row(
+            [print_display(i.title), print_url(i.url), i.body[:30]])
     print display
 
+
 def show_filter(filter):
-	clear()
-	print "\n\n\n"
-	print print_title("{} Issues".format(filter))
-	issues = get_issues(filter,org)
-	display_issues(issues) 
-	print print_msg("a -- assigned issues | m -- mentioned Issues | c -- created issues | s -- subscribed | q -- quit")
+    clear()
+    print "\n\n\n"
+    print print_title("{} Issues".format(filter))
+    issues = get_issues(filter, org)
+    display_issues(issues)
+    print print_msg("a -- assigned issues | m -- mentioned Issues | c -- created issues | s -- subscribed | q -- quit")
+
 
 if __name__ == "__main__":
-	org = gh_init(g)
-	show_filter('assigned')
-	while(1):
-		kp = readkey()
-		if kp == 'q':
-			break
-		elif kp == 'a':
-			show_filter('assigned')
-		elif kp == 'm':
-			show_filter('mentioned')
-		elif kp == 'c':
-			show_filter('created')
-		elif kp == 's':
-			show_filter('subscribed')
-		else:
-			print('Unknown Input --- press a key to continue')
-			cont = readkey()
-			show_filter('assigned')
-			
-
-    
+    org = gh_init(g)
+    show_filter('assigned')
+    while(1):
+        kp = readkey()
+        if kp == 'q':
+            break
+        elif kp == 'a':
+            show_filter('assigned')
+        elif kp == 'm':
+            show_filter('mentioned')
+        elif kp == 'c':
+            show_filter('created')
+        elif kp == 's':
+            show_filter('subscribed')
+        else:
+            print('Unknown Input --- press a key to continue')
+            cont = readkey()
+            show_filter('assigned')
---
+++ tests/issues_tests.py
@@ -0,0 +1,22 @@
+from github import Github
+import issues
+
+
+def test_connect_github():
+    pass
+
+
+def test_assigned_issues():
+    pass
+
+
+def test_mentioned_issues():
+    pass
+
+
+def test_created_issues():
+    pass
+
+
+def test_subscribed_issues():
+    pass
